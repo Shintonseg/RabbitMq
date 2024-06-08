4db27a07fa38fe6fd686c6fde060ab6f720b5ed9
Shinton George <shintonseg5@gmail.com>
2024-06-08 13:29:00 +0530

    added log and updated consumer

--- rabbitMq/src/main/java/com/smart/rabbitMq/consumer/RabbitMqConsumer.java
+++ rabbitMq/src/main/java/com/smart/rabbitMq/consumer/RabbitMqConsumer.java
@@ -1,7 +1,9 @@
 package com.smart.rabbitMq.consumer;
 
 import com.smart.rabbitMq.configuration.RabbitMqConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,10 +16,12 @@ import org.springframework.stereotype.Component;
 **/
 
 @Component
+@Slf4j
 public class RabbitMqConsumer {
 
-    @RabbitListener(queues = RabbitMqConfiguration.QUEUE_NAME)
+    @RabbitListener(queues = {"${spring.queue-name}"})
     public void receiveMessage(String message) {
+        log.info("received message {}", message);
         System.out.println("Consumer Received: " + message);
     }
 }
--- rabbitMq/src/main/java/com/smart/rabbitMq/controller/HelloController.java
+++ rabbitMq/src/main/java/com/smart/rabbitMq/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.smart.rabbitMq.controller;
 
 import com.smart.rabbitMq.publisher.RabbitMqProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class HelloController {
 
     private final RabbitMqProducer rabbitMqProducer;
 
     @GetMapping("/send")
     public String sayHello(@RequestParam String message) {
+        log.info("send message {}",message);
         rabbitMqProducer.send(message);
         return "Message sent to RabbitMQ: " + message;
     }
