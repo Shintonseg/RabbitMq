494abaa6ef9d87cdd7ca758592ab86b2bd657828
Shinton George <shintonseg5@gmail.com>
2024-06-08 13:22:28 +0530

    updated producer send method

--- rabbitMq/src/main/java/com/smart/rabbitMq/publisher/RabbitMqProducer.java
+++ rabbitMq/src/main/java/com/smart/rabbitMq/publisher/RabbitMqProducer.java
@@ -3,16 +3,23 @@ package com.smart.rabbitMq.publisher;
 import com.smart.rabbitMq.configuration.RabbitMqConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RabbitMqProducer {
 
+    @Value("${spring.exchange-name}")
+    private String exchangeName;
+
+    @Value("${spring.routing-key}")
+    private String routingKey;
+
     private final RabbitTemplate rabbitTemplate;
 
     public void send(String message) {
-        rabbitTemplate.convertAndSend(RabbitMqConfiguration.QUEUE_NAME, message);
+        rabbitTemplate.convertAndSend(exchangeName,routingKey,message);
         System.out.println("Message from producer : " + message);
     }
 }
